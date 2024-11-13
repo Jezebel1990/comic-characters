707da21226d97b3aadce31ee77fccbc02d28f369
Jezebel1990 <jezebelguedes@gmail.com>
2024-11-13 00:46:03 -0300

    feat: Add interface home page

---
+++ src/components/HeroToolbar/HeroToolbar.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import hero from "../../assets/superhero.png";
+import toogleLeft from "../../assets/toggle-left.png";
+import toogleRight from "../../assets/toggle-right.png";
+import darkHeart from "../../assets/darkHeart1,5x.png";
+import heart from "../../assets/heart1,5x.png";
+import "./HeroToolbar.css";
+
+interface HeroToolbarProps {
+    totalHeroes: number;
+    onToggleSort: () => void;
+    isSortActive: boolean;
+    onShowFavorites: () => void;
+    isFavoritesActive: boolean;
+  }
+  
+
+export function HeroToolbar({
+    totalHeroes,
+  onToggleSort,
+  isSortActive,
+  onShowFavorites,
+  isFavoritesActive
+}: HeroToolbarProps) {
+  const [isFavorited, setIsFavorited] = useState(isFavoritesActive);
+
+  const handleFavoriteClick = () => {
+    setIsFavorited(!isFavorited);
+    onShowFavorites(); 
+  };
+
+    return (
+        <div className="hero-toolbar">
+        <div className="hero-toolbar-left">
+        <span>Encontrados {totalHeroes} heróis</span>
+        </div>
+
+        <div className="hero-toolbar-right">
+        <img src={hero} alt="Ícone de herói" className='hero-icon'/>
+        <button onClick={onToggleSort} className="sort-button" style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
+          <h5>Ordenar por nome - A/Z</h5>
+          <img
+            src={isSortActive ? toogleLeft : toogleRight} 
+            alt="Ícone de toggle de ordenação"
+            className="toggle-icon"
+          />
+        </button>
+  
+        <button onClick={handleFavoriteClick} 
+        style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+         className={`favorites-button ${isFavorited ? 'active' : ''}`}>
+          <img src={isFavorited ? darkHeart : heart} alt="Ícone de favoritos" className='favorites-icon'/>
+          <h5>Somente favoritos</h5>
+        </button>
+      </div>
+      </div>
+    );
+  }
+  
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,26 @@
+import { useFetchCharacters } from "../../hooks/useFetchCharacters";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 import './Home.css';
+import { LikeButton } from "../../components/LikeButton/LikeButton";
+import { HeroToolbar } from "../../components/HeroToolbar/HeroToolbar";
+import  { useState } from 'react';
 
 export function Home() {
-   
+  const [isSortActive, setIsSortActive] = useState(false);
+  const [isFavoritesActive, setIsFavoritesActive] = useState(false);
+  
+
+  const handleToggleSort = () => {
+    setIsSortActive(!isSortActive);
+  };
+
+  const handleShowFavorites = () => {
+    setIsFavoritesActive(!isFavoritesActive);
+  };
+  const { characters, loading, error } = useFetchCharacters();
+  const totalHeroes = characters ? characters.length : 0; 
+
     return (
     <>
      <Navbar/>
@@ -13,6 +30,37 @@ export function Home() {
       </main>
       <div className="search-bar-container">
       <SearchBar/>
+      </div>
+
+      <div className="hero-toolbar-container">
+      <HeroToolbar
+        totalHeroes={totalHeroes}
+        onToggleSort={handleToggleSort}
+        isSortActive={isSortActive}
+        onShowFavorites={handleShowFavorites}
+        isFavoritesActive={isFavoritesActive}
+      />
+    </div>
+
+    <div className="container">
+      <div className="character-list">
+        {loading && <p>Carregando...</p>}
+        {error && <p>Erro: {error}</p>}
+
+        {characters.map(character => (
+          <div key={character.id} className="character-card">
+            <img src={`${character.image}`} 
+            alt={character.name} 
+            loading="lazy"
+             className="character-image"
+            />
+             <div className="character-name-container">
+            <h5 className="character-name">{character.name}</h5>
+              <LikeButton />
+            </div>
+          </div>
+        ))}
+      </div>
       </div>
     </>
     );
